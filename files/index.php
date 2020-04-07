6e3c204f210e70244f9422fea0eb486c1d0473c9
TheSwordBreaker <kunalguptadev007@gmail.com>
2020-04-08 00:03:01 +0530

    hhdd

--- files/index.php
+++ files/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: GET,POST, OPTIONS, PUT");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 require("main.php");
--- user/index.php
+++ user/index.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: GET , POST, OPTIONS, PUT");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
