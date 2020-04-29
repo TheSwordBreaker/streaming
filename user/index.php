ba79ff78c2ea655d2ef8dbf1a5ebf003928c3533
TheSwordBreaker <kunalguptadev007@gmail.com>
2020-04-29 13:05:44 +0530

    ss

--- user/index.php
+++ user/index.php
@@ -1,10 +1,10 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET , POST, OPTIONS, PUT");
-header("Access-Control-Allow-Credentials: true");
-header('Content-Type: application/json');
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: access");
+// header("Access-Control-Allow-Methods: GET , POST, OPTIONS, PUT");
+// header("Access-Control-Allow-Credentials: true");
+// header('Content-Type: application/json');
 
 include_once('main.php');
 $auth = new auth();
