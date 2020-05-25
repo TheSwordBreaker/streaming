6b4a3a679754bad37dfe92597ca1da3a9c8a4f92
TheSwordBreaker <kunalguptadev007@gmail.com>
2020-05-25 11:13:45 +0530

    Tools setup added

---
+++ toolSetup.sh
@@ -0,0 +1,18 @@
+# sudo apt-get update
+# sudo apt-get remove docker docker-engine docker.io
+
+echo "Installing Docker"
+sudo apt install docker.io -y
+
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ${USER}
+docker --version
+
+echo "Installing Docker-compose"
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo "Done"
